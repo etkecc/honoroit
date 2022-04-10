5e77cd2527d9e995699efd2498a45aeb185c477e
Aine <aine@etke.cc>
2022-04-10 17:50:26 +0300

    updated defaults wording

--- config/defaults.go
+++ config/defaults.go
@@ -11,10 +11,10 @@ var defaultConfig = &Config{
 	Text: Text{
 		PrefixOpen: "[OPEN]",
 		PrefixDone: "[DONE]",
-		Greetings:  "Hello\nyour message was sent to developers. Please, keep calm and wait for answer, usually it takes 1-2 days.",
+		Greetings:  "Hello\nyour message was sent to operators. Please, keep calm and wait for answer, it takes 1-2 days.\nDon't forget that instant messenger is the same communication channel as email, so don't expect an instant response.",
 		Error:      "Something is wrong.\nI already notified developers, they're fixing the issue.\n\nPlease, try again later or use any other contact method.",
 		EmptyRoom:  "Customer left the room.\nConsider that request as closed.",
 		Start:      "Customer has been invited to the new room. Send messages into that thread and they will be automatically forwarded.",
-		Done:       "Developer marked your request as completed.\nIf you think that it's not done yet, please, start another 1:1 chat with me to open a new request.",
+		Done:       "Operator marked your request as completed.\nIf you think that it's not done yet, please start another 1:1 chat with me to open a new request.",
 	},
 }

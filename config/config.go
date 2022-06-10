05af24a40da44a6ca5bab0fcd673dbce49b2dbf5
Aine <aine@etke.cc>
2022-06-10 22:54:53 +0300

    replace env parser with external lib

--- config/config.go
+++ config/config.go
@@ -1,70 +1,39 @@
 package config
 
 import (
-	"os"
-	"strconv"
-	"strings"
+	"gitlab.com/etke.cc/go/env"
 )
 
 const prefix = "honoroit"
 
-func env(shortkey string, defaultValue string) string {
-	key := strings.ToUpper(prefix + "_" + strings.ReplaceAll(shortkey, ".", "_"))
-	value := strings.TrimSpace(os.Getenv(key))
-	if value == "" {
-		return defaultValue
-	}
-
-	return value
-}
-
-func envInt(shortkey string, defaultValue int) int {
-	str := env(shortkey, "")
-	if str == "" {
-		return defaultValue
-	}
-
-	val, err := strconv.Atoi(str)
-	if err != nil {
-		return defaultValue
-	}
-
-	return val
-}
-
-func envBool(shortkey string) bool {
-	key := strings.ToUpper(prefix + "_" + strings.ReplaceAll(shortkey, ".", "_"))
-	value := strings.TrimSpace(os.Getenv(key))
-	return (value == "1" || value == "true" || value == "yes")
-}
-
 // New config
 func New() *Config {
+	env.SetPrefix(prefix)
 	return &Config{
-		Homeserver:   env("homeserver", defaultConfig.Homeserver),
-		RoomID:       env("roomid", defaultConfig.RoomID),
-		Login:        env("login", defaultConfig.Login),
-		Password:     env("password", defaultConfig.Password),
-		Sentry:       env("sentry", defaultConfig.Sentry),
-		LogLevel:     env("loglevel", defaultConfig.LogLevel),
-		CacheSize:    envInt("cachesize", defaultConfig.CacheSize),
-		NoEncryption: envBool("noencryption"),
-		Prefix:       env("prefix", defaultConfig.Prefix),
+		Homeserver:   env.String("homeserver", defaultConfig.Homeserver),
+		RoomID:       env.String("roomid", defaultConfig.RoomID),
+		Login:        env.String("login", defaultConfig.Login),
+		Password:     env.String("password", defaultConfig.Password),
+		Sentry:       env.String("sentry", defaultConfig.Sentry),
+		LogLevel:     env.String("loglevel", defaultConfig.LogLevel),
+		CacheSize:    env.Int("cachesize", defaultConfig.CacheSize),
+		NoEncryption: env.Bool("noencryption"),
+		Prefix:       env.String("prefix", defaultConfig.Prefix),
 		DB: DB{
-			DSN:     env("db.dsn", defaultConfig.DB.DSN),
-			Dialect: env("db.dialect", defaultConfig.DB.Dialect),
+			DSN:     env.String("db.dsn", defaultConfig.DB.DSN),
+			Dialect: env.String("db.dialect", defaultConfig.DB.Dialect),
 		},
 		Text: Text{
-			PrefixOpen: env("text.prefix.open", defaultConfig.Text.PrefixOpen),
-			PrefixDone: env("text.prefix.done", defaultConfig.Text.PrefixDone),
-			Greetings:  env("text.greetings", defaultConfig.Text.Greetings),
-			Join:       env("text.join", defaultConfig.Text.Join),
-			Invite:     env("text.invite", defaultConfig.Text.Invite),
-			Leave:      env("text.leave", defaultConfig.Text.Leave),
-			Error:      env("text.error", defaultConfig.Text.Error),
-			EmptyRoom:  env("text.emptyroom", defaultConfig.Text.EmptyRoom),
-			Start:      env("text.start", defaultConfig.Text.Start),
-			Done:       env("text.done", defaultConfig.Text.Done),
+			PrefixOpen: env.String("text.prefix.open", defaultConfig.Text.PrefixOpen),
+			PrefixDone: env.String("text.prefix.done", defaultConfig.Text.PrefixDone),
+			Greetings:  env.String("text.greetings", defaultConfig.Text.Greetings),
+			Join:       env.String("text.join", defaultConfig.Text.Join),
+			Invite:     env.String("text.invite", defaultConfig.Text.Invite),
+			Leave:      env.String("text.leave", defaultConfig.Text.Leave),
+			Error:      env.String("text.error", defaultConfig.Text.Error),
+			EmptyRoom:  env.String("text.emptyroom", defaultConfig.Text.EmptyRoom),
+			Start:      env.String("text.start", defaultConfig.Text.Start),
+			Done:       env.String("text.done", defaultConfig.Text.Done),
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/lib/pq v1.10.4
 	github.com/mattn/go-sqlite3 v1.14.13
 	github.com/stretchr/testify v1.7.1
+	gitlab.com/etke.cc/go/env v1.0.0
 	gitlab.com/etke.cc/go/logger v1.0.0
 	gitlab.com/etke.cc/linkpearl v0.0.0-20220531091432-f61370b94f6b
 	maunium.net/go/mautrix v0.11.0

a95f11a914be0e028ea335b501085e0587cb6c0d
Aine <aine@etke.cc>
2022-12-24 13:40:57 +0200

    remove ru texts

--- config/defaults.go
+++ config/defaults.go
@@ -23,12 +23,7 @@ Don't forget that instant messenger is the same communication channel as email, 
 [DE] Hallo,
 Ihre Nachricht wurde an die Betreiber gesendet.
 Bitte bewahren Sie Ruhe und warten Sie auf die Antwort (normalerweise dauert es 1-2 Tage).
-Vergessen Sie nicht, dass Instant Messenger derselbe Kommunikationskanal wie E-Mail ist, erwarten Sie also nicht sofort eine Antwort.
-
-[RU] Привет,
-Ваше сообщение было отправлено операторам.
-Пожалуйста, наберитесь терпения и дождитесь ответа (обычно это занимает 1-2 дня).
-Имейте в виду, что мессенджер это такой же канал связи, как и email, так что не стоит ожидать мгновенного ответа.`,
+Vergessen Sie nicht, dass Instant Messenger derselbe Kommunikationskanal wie E-Mail ist, erwarten Sie also nicht sofort eine Antwort.`,
 		Join:   "New customer (%s) joined the room",
 		Invite: "Customer (%s) invited another user (%s) into the room",
 		Leave:  "Customer (%s) left the room",
@@ -38,28 +33,18 @@ Please, try again later or use any other contact method.
 
 [DE] Irgendetwas stimmt nicht.
 Ich habe die Entwickler benachrichtigt und sie sind dabei, das Problem zu beheben.
-Bitte versuchen Sie es später noch einmal oder verwenden Sie eine andere Kontaktmethode.
-
-[RU] Что-то пошло не так.
-Разрабочтики уже в курсе проблемы и заняты ее решением.
-Пожалуйста, попробуйте еще раз позже или используйте любой другой способ связи.`,
+Bitte versuchen Sie es später noch einmal oder verwenden Sie eine andere Kontaktmethode.`,
 		EmptyRoom: "The last customer left the room.\nConsider that request closed.",
 		Start:     "The customer was invited to the new room. Send messages into that thread and they will be automatically forwarded.",
 		Done: `[EN] The operator marked your request as completed.
 If you think that it's not done yet, please start another 1:1 chat with me to open a new request.
 
 [DE] Der Betreiber hat Ihre Anfrage als abgeschlossen markiert.
-Wenn Sie der Meinung sind, dass es noch nicht fertig ist, starten Sie bitte einen weiteren 1:1-Chat mit mir, um eine neue Anfrage zu öffnen.
-
-[RU] Оператор отметил Ваш запрос как решенный.
-Если Вы считаете что он еще не решен, пожалуйста, начните еще один 1:1 чат со мной для открытия нового запроса.`,
+Wenn Sie der Meinung sind, dass es noch nicht fertig ist, starten Sie bitte einen weiteren 1:1-Chat mit mir, um eine neue Anfrage zu öffnen.`,
 		NoEncryption: `[EN] Unfortunately, encryption is disabled to prevent common decryption issues among customers.
 Please, start a new un-encrypted chat with me.
 
 [DE] Leider ist die Verschlüsselung deaktiviert, um häufige Entschlüsselungsprobleme bei den Kunden zu vermeiden.
-Bitte beginnen Sie einen neuen unverschlüsselten Chat mit mir.
-
-[RU] К сожалению, шифрование отключено чтобы предотвратить частые ошибки с дешифровкой у клиентов.
-Пожалуйста, начните новый, незашифрованный чат со мной.`,
+Bitte beginnen Sie einen neuen unverschlüsselten Chat mit mir.`,
 	},
 }
